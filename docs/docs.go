d234848c9d05e2b0a03da61e264d59c94005394b
DESOLATE17 <taktashovadasha@yandex.ru>
2024-04-07 17:29:07 +0300

    swagger

--- docs/docs.go
+++ docs/docs.go
@@ -518,7 +518,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "http://158.160.2.243:8080",
+	Host:             "158.160.2.243:8080",
 	BasePath:         "/",
 	Schemes:          []string{"http", "https"},
 	Title:            "Tiflo_Backend",
