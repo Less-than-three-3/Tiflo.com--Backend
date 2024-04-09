e0b968d39cd658a7c23bd982a1183259113f5ac3
DESOLATE17 <taktashovadasha@yandex.ru>
2024-04-09 23:36:02 +0300

    fixed swagger

--- cmd/main.go
+++ cmd/main.go
@@ -12,7 +12,7 @@ import (
 // @version 1.0
 // @description App for working with audio descriptions(tiflocomments)
 
-// @host 158.160.2.243:8080
+// @host tiflo.tech
 // @schemes http https
 // @BasePath /
 func main() {
