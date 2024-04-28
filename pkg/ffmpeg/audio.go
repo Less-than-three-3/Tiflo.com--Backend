fb824abecc38e24a7e10b5a2779d49ffb79249d8
IvanStukalov <ivanstukalov2003@gmail.com>
2024-04-29 01:14:39 +0300

    fixed split point to time.Duration

--- pkg/ffmpeg/audio.go
+++ pkg/ffmpeg/audio.go
@@ -38,16 +38,17 @@ func (s *MediaServiceImpl) SplitAudio(audioPartToSplit model.AudioPart, splitPoi
 	startStr := s.convertTimeToString(start)
 
 	splitPoint := s.ConvertTimeFromString(splitPointStr)
+	splitPointDuration := time.Duration(splitPoint)
 	s.logger.Info("splitPoint: ", splitPoint)
 
 	s.logger.Info("ffmpeg", "-i", audioPartToSplit.Path, "-vn", "-acodec", "pcm_s16le",
-		"-ss", startStr, "-t", fmt.Sprintf("%02d:%02d:%02d.%03d", int(duration.Hours()), int(duration.Minutes())%60,
-			int(duration.Seconds())%60, int(duration.Milliseconds())%60),
+		"-ss", startStr, "-t", fmt.Sprintf("%02d:%02d:%02d.%03d", int(splitPointDuration.Hours()), int(splitPointDuration.Minutes())%60,
+			int(splitPointDuration.Seconds())%60, int(splitPointDuration.Milliseconds())%60),
 		s.pathForMedia+firstPartName.String()+".wav")
 
 	_, err := exec.Command("ffmpeg", "-i", audioPartToSplit.Path, "-vn", "-acodec", "pcm_s16le",
-		"-ss", startStr, "-t", fmt.Sprintf("%02d:%02d:%02d.%03d", int(duration.Hours()), int(duration.Minutes())%60,
-			int(duration.Seconds())%60, int(duration.Milliseconds())%60),
+		"-ss", startStr, "-t", fmt.Sprintf("%02d:%02d:%02d.%03d", int(splitPointDuration.Hours()), int(splitPointDuration.Minutes())%60,
+			int(splitPointDuration.Seconds())%60, int(splitPointDuration.Milliseconds())%60),
 		s.pathForMedia+firstPartName.String()+".wav").Output()
 	if err != nil {
 		return nil, err
